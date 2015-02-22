90724a45317d66e7ab6dfbbccb4cc44f3d3eda51
Eric Sayler <esayler@colgate.edu>
2015-02-21 21:16:50 -0500

    add sorting capability to index view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.sorted_by("name")
+    if params[:sort_by] != nil
+      @products = Product.sorted_by(params[:sort_by])
+    else
+      @products = Product.sorted_by("name")
+    end
   end
 
   def show
