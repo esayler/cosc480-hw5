404257eb7f0aa8667620d4bde4fd400228727a22
Eric Sayler <esayler@colgate.edu>
2015-02-22 04:20:27 -0500

    add create/update product pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,35 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    if @product = Product.create(create_update_params)
+      flash[:notice] = "New product #{@product.name} created successfully"
+      redirect_to products_path
+    else
+      flash[:alert] = "Error in creating product!"
+      redirect_to new_product_path
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(create_update_params)
+      flash[:notice] = "Product updated successfully!"
+      redirect_to product_path(@product)
+    else
+      flash[:alert] = "Error: Product wasn't updated successfully!"
+      redirect_to edit_product_path(@product)
+    end
+  end
+
   private
 
   def create_update_params
